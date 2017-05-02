71f0f785f16517f116529bd312f0c826103c1572
Edward Lloyd Dickenson <EdwardLDickenson@users.noreply.github.com>
2017-05-01 23:17:00 -0500

    Add files via upload

--- todo.js
+++ todo.js
@@ -2,12 +2,6 @@ window.onload = function() {
   console.log("JavaScript file loaded correctly");
 }
 
-//  Once again, using these iterators is a C like stylistic choice
-//  Another approch might be to use the todo item as a key in a map, and pair
-//  the item with a bool. But if I did that, I'd probably have to check for
-//  uniqueness.
-
-var todoIterator = 0;
 var app = angular.module("todo", []);
 app.controller("ctrl", function($scope){
 
@@ -18,32 +12,39 @@ app.controller("ctrl", function($scope){
     //  Do not add items which are empty to the list or database
     if($scope.todoBody != "" && $scope.todoBody != undefined)
     {
-      $scope.item = {"todo": $scope.todoBody, "checked": false,
-      "pos": todoIterator};
+      $scope.item = {"todo": $scope.todoBody, "checked": false};
       $scope.todoItems.push($scope.item);
       console.log("Add new item: \"" + $scope.todoBody + "\"");
-      ++todoIterator;
-      console.log("todoIterator: " + String(todoIterator));
     }
   }
 
   $scope.checked = function(arg){
 
     //  Push the item to the other list
-    $scope.finishedItems.push($scope.todoItems[arg.pos]);
+    if($scope.todoItems.length == 1)
+    {
+      $scope.finishedItems.push($scope.todoItems[0]);
+    }
+    else
+    {
+      //  I started out by using an iterator, but that created a lot of manual
+      //  work.  So I just decided to let the JS do that work and use indexOf.
+      $scope.finishedItems.push($scope.todoItems[$scope.todoItems.indexOf(arg)]);
+    }
 
-    //  Remove the item from the first list
     if($scope.todoItems.length == 1)
     {
       $scope.todoItems.splice(0, 1);
     }
-    $scope.todoItems.splice(arg.pos, 1);
 
-    //  Flip the true/false value, de/increment iterators and log to console
+    else
+    {
+      $scope.todoItems.splice($scope.todoItems.indexOf(arg), 1);
+    }
+
+    //  Flip the true/false value and log to console
     arg.checked = !arg.checked;
     console.log("\"" + arg.todo + "\" was checked and is now: " +
     String(arg.checked));
-    --todoIterator;
-    console.log("todoIterator: " + String(todoIterator));
   }
 });
